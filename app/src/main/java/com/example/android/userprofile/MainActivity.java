9f9327a0a7c18d59677ece8e3dbf9c477f9982d2
liwona <liwona@users.noreply.github.com>
2018-03-04 20:15:05 +0100

    userProfile final commit

--- app/src/main/java/com/example/android/userprofile/MainActivity.java
+++ app/src/main/java/com/example/android/userprofile/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.userprofile;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView picture = (ImageView) findViewById(R.id.profile_picture);
+        picture.setImageResource(R.drawable.picture);
+
+        TextView name = (TextView) findViewById(R.id.name);
+        name.setText(String.valueOf("Iwona"));
+
+        name.setTextSize(30);
+
+        name.setTextColor(Color.parseColor("#000000"));
+
+
+        TextView birthday = (TextView) findViewById(R.id.birthday);
+        birthday.setText(String.valueOf("01/01/1990"));
+
+        birthday.setTextSize(20);
+
+        birthday.setTypeface(null, Typeface.BOLD);
+
+        TextView country = (TextView) findViewById(R.id.country);
+        country.setText(String.valueOf("Poland"));
+
+        country.setTextSize(20);
+
+        country.setTypeface(null, Typeface.BOLD_ITALIC);
+
     }
 }
